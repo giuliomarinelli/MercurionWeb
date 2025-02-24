03a5294c791f6d2da4cdc2f96e3ffb2abde1de3b
giuliomarinelli <giulio.marinelli@icloud.com>
2025-02-24 23:21:15 +0100

    NEST | fix

--- spectraserver/src/app_modules/auth/services/revoked-token.service.ts
+++ spectraserver/src/app_modules/auth/services/revoked-token.service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { UUID } from 'crypto';
 import { RedisService } from 'src/app_modules/redis/services/redis.service';
 
 
 @Injectable()
 export class RevokedTokenService {
-    
-  constructor(private readonly redisService: RedisService) {}
 
-  /**
-   * Revoca un token salvandolo in Redis con un TTL fisso
-   * @param token Token JWT da revocare
-   * @param expiresIn Durata standard del token in secondi (es. 3600 per 1h)
-   */
-  async revokeToken(token: string, expiresIn: number): Promise<void> {
-    await this.redisService.set(`revoked:${token}`, 'true', expiresIn);
-  }
+    constructor(private readonly redisService: RedisService) { }
 
-  /**
-   * Controlla se un token è stato revocato
-   * @param token Token JWT da verificare
-   * @returns True se il token è revocato
-   */
-  async isTokenRevoked(token: string): Promise<boolean> {
-    return !!(await this.redisService.get(`revoked:${token}`));
-  }
+    /**
+     * Revoca un token salvandolo in Redis con un TTL fisso
+     * @param token Token JWT da revocare
+     * @param expiresIn Durata standard del token in secondi (es. 3600 per 1h)
+     */
+    async revokeToken(jti: UUID, expiresIn: number): Promise<void> {
+        await this.redisService.set(`revoked:${jti.toString()}`, 'true', expiresIn);
+    }
+
+    /**
+     * Controlla se un token è stato revocato
+     * @param token Token JWT da verificare
+     * @returns True se il token è revocato
+     */
+    async isTokenRevoked(token: string): Promise<boolean> {
+        return !!(await this.redisService.get(`revoked:${token}`));
+    }
 }
