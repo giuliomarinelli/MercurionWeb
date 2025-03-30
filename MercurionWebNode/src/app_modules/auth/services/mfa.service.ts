825a237de4c7c4b1cf3f41ec59b09dc787bef67b
giuliomarinelli <giulio.marinelli@icloud.com>
2025-03-30 12:16:25 +0200

    NEST | get enabled mfa strategies

--- MercurionWebNode/src/app_modules/auth/services/mfa.service.ts
+++ MercurionWebNode/src/app_modules/auth/services/mfa.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UUID } from 'crypto';
 import { SercurityService } from './sercurity.service';
 import { UserService } from 'src/app_modules/user/services/user.service';
+import { MfaStrategy } from 'src/app_modules/user/Models/enums/mfa-strategy.enum';
 
 @Injectable()
 export class MfaService {
@@ -13,6 +14,10 @@ export class MfaService {
 
     public async isMfaEnabled(userId: UUID): Promise<boolean> {
         return !!(await this.userService.getUserEnabledMfaStrategies(userId)).length
+    }
+
+    public async getEnabledMfaStrategies(userId: UUID): Promise<MfaStrategy[]> {
+        return await this.userService.getUserEnabledMfaStrategies(userId)
     }
 
 }
