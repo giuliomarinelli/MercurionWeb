54707177b80add03004d015af103320b90188f4b
giuliomarinelli <giulio.marinelli@icloud.com>
2025-04-08 19:24:36 +0200

    NG | created colors palette

--- MercurionWebNg/tailwind.config.js
+++ MercurionWebNg/tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{html,ts,scss}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          'surface-main': '#F9FAFB',
+          'surface-secondary': '#F3F4F6',
+          'on-surface-main': '#111827',
+          'on-surface-secondary': '#4B5563',
+          'accent-primary': '#2563EB',
+          'accent-secondary': '#10B981',
+          'warning': '#F59E0B',
+          'error': '#F43F5E',
+          'border': '#E5E7EB',
+        },
+        dark: {
+          'surface-main': '#111827',
+          'surface-secondary': '#1F2937',
+          'on-surface-main': '#F3F4F6',
+          'on-surface-secondary': '#9CA3AF',
+          'accent-primary': '#60A5FA',
+          'accent-secondary': '#34D399',
+          'warning': '#FBBF24',
+          'error': '#FB7185',
+          'border': '#374151',
+        }
+      }
+    },
   },
   plugins: [],
 }
