8e44315ace8f3abe91f347cd60372d1c46f56716
giuliomarinelli <giulio.marinelli@icloud.com>
2025-04-28 19:37:23 +0200

    NEST | Creating MoleculeDetailView

--- MercurionWebNode/src/app_modules/meilisearch/services/molecule-sync.service.ts
+++ MercurionWebNode/src/app_modules/meilisearch/services/molecule-sync.service.ts
@@ -1,75 +1,82 @@
 import { RecordAny } from './../../../../node_modules/meilisearch/dist/types/types/types.d';
 import { MoleculePreviewDBView } from './../../chembl/Models/entities/molecule-preview-db-view.entity';
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, OnModuleInit, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Index, MeiliSearch } from 'meilisearch';
 
 @Injectable()
-export class MoleculeSyncService {
-  
-  private index: Index<RecordAny>
+export class MoleculeSyncService implements OnModuleInit {
 
-  constructor(
-    @InjectRepository(MoleculePreviewDBView, 'ChemblDB')
-    private readonly moleculeRepo: Repository<MoleculePreviewDBView>,
+    private readonly logger = new Logger(MoleculeSyncService.name)
 
-    @Inject('MEILISEARCH_CLIENT')
-    private readonly meiliClient: MeiliSearch,
-  ) {
-    this.index = this.meiliClient.index('molecules')
-  }
+    private index: Index<RecordAny>
 
-  async syncAllMoleculesWithProgress(
-    onProgress: (progress: { synced: number; total: number }) => void, 
-    batchSize = 5000
-  ) {
-    const total = await this.moleculeRepo.count();
-    console.log(`🔵 Total molecules to sync: ${total}`)
+    constructor(
+        @InjectRepository(MoleculePreviewDBView, 'ChemblDB')
+        private readonly moleculeRepo: Repository<MoleculePreviewDBView>,
 
-    let offset = 0;
-    let synced = 0;
-
-    while (synced < total) {
-      const batch = await this.moleculeRepo.find({
-        skip: offset,
-        take: batchSize,
-      });
-
-      console.log(`🟢 Loaded batch of size: ${batch.length} (offset: ${offset})`)
-
-      if (batch.length === 0) {
-        console.log('🛑 No more molecules to sync, exiting loop.')
-        break;
-      }
-
-      await this.index.addDocuments(batch, { primaryKey: 'id' })
-
-      synced += batch.length;
-      offset += batchSize;
-
-      onProgress({ synced, total })
+        @Inject('MEILISEARCH_CLIENT')
+        private readonly meiliClient: MeiliSearch,
+    ) {
+        this.index = this.meiliClient.index('molecules')
     }
-  }
-
-  async createIndexIfNotExists() {
-    const indexes = await this.meiliClient.getIndexes()
-    const exists = indexes.results.find(idx => idx.uid === 'molecules')
-
-    if (!exists) {
-      console.log('🔵 Creating molecules index...');
-      await this.meiliClient.createIndex('molecules', {
-        primaryKey: 'id',
-      });
-
-      await this.index.updateSettings({
-        searchableAttributes: ['preferredName', 'synonyms', 'cmbId', 'smiles'], 
-        filterableAttributes: ['maxPhase', 'mwFreebase', 'alogp', 'moleculeType', 'oralAdmin', 'parenteralAdmin', 'topicalAdmin'],
-      });
-
-      console.log('✅ Molecules index created and configured.')
-    } else {
-      console.log('🔵 Molecules index already exists.')
+    onModuleInit() {
+        this.createIndexIfNotExists()
     }
-  }
+
+
+
+    async syncAllMoleculesWithProgress(
+        onProgress: (progress: { synced: number; total: number }) => void,
+        batchSize = 5000
+    ) {
+        const total = await this.moleculeRepo.count();
+        console.log(`🔵 Total molecules to sync: ${total}`)
+
+        let offset = 0;
+        let synced = 0;
+
+        while (synced < total) {
+            const batch = await this.moleculeRepo.find({
+                skip: offset,
+                take: batchSize,
+            });
+
+            console.log(`🟢 Loaded batch of size: ${batch.length} (offset: ${offset})`)
+
+            if (batch.length === 0) {
+                this.logger.log('🛑 No more molecules to sync, exiting loop.')
+                break;
+            }
+
+            await this.index.addDocuments(batch, { primaryKey: 'id' })
+
+            synced += batch.length
+            offset += batchSize
+
+            onProgress({ synced, total })
+        }
+    }
+
+    async createIndexIfNotExists() {
+        const indexes = await this.meiliClient.getIndexes()
+        const exists = indexes.results.find(idx => idx.uid === 'molecules')
+
+        if (!exists) {
+            this.logger.log('🔵 Creating molecules index...');
+            await this.meiliClient.createIndex('molecules', {
+                primaryKey: 'id',
+            });
+
+            await this.index.updateSettings({
+                searchableAttributes: ['preferredName', 'synonyms', 'cmbId', 'smiles'],
+                filterableAttributes: ['maxPhase', 'mwFreebase', 'alogp', 'moleculeType', 'oralAdmin', 'parenteralAdmin', 'topicalAdmin'],
+            });
+
+            this.logger.log('✅ Molecules index created and configured.')
+        } else {
+            this.logger.log('🔵 Molecules index already exists.')
+        }
+    }
 }
