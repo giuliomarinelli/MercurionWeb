e5d00f6547589c0e897e5766658f559df7af9acc
giuliomarinelli <giulio.marinelli@icloud.com>
2025-05-01 12:13:46 +0200

    NG | design optimization

--- MercurionWebNg/src/app/app.component.ts
+++ MercurionWebNg/src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
   title = 'MercurionWebNg'
 
   smilesString = 'CC(=O)OC1=CC=CC=C1C(=O)O'
-  // smilesString = 'CC[C@@H]([C@H](C)O)N1C(=O)N(C=N1)C2=CC=C(C=C2)N3CCN(CC3)C4=CC=C(C=C4)OC[C@H]5C[C@](OC5)(CN6C=NC=N6)C7=C(C=C(C=C7)F)F'
+  smilesString1 = 'CC[C@@H]([C@H](C)O)N1C(=O)N(C=N1)C2=CC=C(C=C2)N3CCN(CC3)C4=CC=C(C=C4)OC[C@H]5C[C@](OC5)(CN6C=NC=N6)C7=C(C=C(C=C7)F)F'
 
   isDarkTheme: Signal<boolean> = computed(() => this.themeManagerService.theme() === 'dark')
 
