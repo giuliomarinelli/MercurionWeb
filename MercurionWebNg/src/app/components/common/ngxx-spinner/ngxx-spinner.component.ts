3a34e5f089b26785882c10b6f2687e0d28f89e3a
giuliomarinelli <giulio.marinelli@icloud.com>
2025-05-03 22:35:07 +0200

    NG | optimized spinner contrast

--- MercurionWebNg/src/app/components/common/ngxx-spinner/ngxx-spinner.component.ts
+++ MercurionWebNg/src/app/components/common/ngxx-spinner/ngxx-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, effect, Input, OnInit } from '@angular/core';
 import { ThemeManagerService } from '../../../services/stores/theme-manager.service';
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 import { LoadingContextService } from '../../../services/stores/loading-context.service';
@@ -11,27 +11,31 @@ import { LoadingContextService } from '../../../services/stores/loading-context.
 })
 export class NgxxSpinnerComponent implements OnInit {
 
+  private bdColor!: string
+
   constructor(
     private readonly themeManager: ThemeManagerService,
     private readonly spinner: NgxSpinnerService,
     protected readonly loadingContext: LoadingContextService
-  ) { }
+  ) {
+    effect(() => {
+      this.bdColor = `rgba(0, 0, 0, ${this.themeManager.theme() === 'light' ? '0.25' : '0.625'})`
+      console.log(this.bdColor)
+    })
+   }
 
   private darkThemeColor: string = '#60A5FA'
   private lightThemeColor: string = '#2563EB'
 
-  @Input() isLoading: boolean = true
-  @Input() isGlobal: boolean = true
-
   ngOnInit(): void {
-    queueMicrotask(() =>
+    queueMicrotask(() => {
       this.spinner.show('globalSpinner', {
         type: 'ball-atom',
         size: 'medium',
-        bdColor: 'rgba(0, 0, 0, 0.6)',
+        bdColor: this.bdColor,
         color: this.themeManager.theme() === 'light' ? this.lightThemeColor : this.darkThemeColor,
       })
-    )
+    })
     // setTimeout(() => this.isLoading = false, 5000)
   }
 
