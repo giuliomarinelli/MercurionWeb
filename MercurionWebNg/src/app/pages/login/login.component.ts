0f9dd2a95fa31044f6c2f41f3481d7f0bd818829
giuliomarinelli <giulio.marinelli@icloud.com>
2025-05-05 21:19:02 +0200

    NG | \login page fisrt stable template version

--- MercurionWebNg/src/app/pages/login/login.component.ts
+++ MercurionWebNg/src/app/pages/login/login.component.ts
@@ -1,20 +1,34 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit, signal } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [NgIf, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
 
-  protected loginForm!: FormGroup
+  protected loginForm!: FormGroup<any>
+
+  protected step = signal<1 | 2>(1)
 
   constructor(
     private readonly fb: FormBuilder
   ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: this.fb.control(null, [Validators.required, Validators.email]),
+      password: this.fb.control(null, [Validators.required])
+    })
+  }
+
+  goToPasswordStep(): void {}
+
+  onSubmit(): void {}
 
 
 
