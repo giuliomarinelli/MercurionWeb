0f5277b71abaefd86b873951af1b6c1b273b1e70
giuliomarinelli <giulio.marinelli@icloud.com>
2025-05-10 19:56:44 +0200

    NG | MoleculeHeaderComponent implementation

--- MercurionWebNg/src/app/components/molecule-detail/molecule-header/molecule-header.component.ts
+++ MercurionWebNg/src/app/components/molecule-detail/molecule-header/molecule-header.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-molecule-header',
-  imports: [],
-  templateUrl: './molecule-header.component.html',
-  styleUrl: './molecule-header.component.css'
+  selector: 'molecule-header',
+  standalone: true,
+  template: `
+    <header>
+      <h1>{{ name() }}</h1>
+      <p>ChEMBL ID: {{ chemblId() }}</p>
+    </header>
+  `,
 })
 export class MoleculeHeaderComponent {
+  private readonly _nameSignal = signal<string>('')
+  private readonly _chemblIdSignal = signal<string>('')
 
+  @Input()
+  set nameInput(value: string) {
+    this._nameSignal.set(value);
+  }
+  readonly name = this._nameSignal.asReadonly()
+
+  @Input()
+  set chemblIdInput(value: string) {
+    this._chemblIdSignal.set(value)
+  }
+  readonly chemblId = this._chemblIdSignal.asReadonly()
 }
--- MercurionWebNg/src/app/pages/molecule-detail/molecule-detail.component.ts
+++ MercurionWebNg/src/app/pages/molecule-detail/molecule-detail.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoleculeService } from '../../services/molecule.service';
+import { switchMap, Observable } from 'rxjs';
+import { MoleculeDetail } from '../../Models/graphql/molecule.detail';
 
 @Component({
   selector: 'app-molecule-detail',
+  standalone: true,
   imports: [],
   templateUrl: './molecule-detail.component.html',
-  styleUrl: './molecule-detail.component.css'
+  styleUrls: ['./molecule-detail.component.css']
 })
-export class MoleculeDetailComponent {
+export class MoleculeDetailComponent implements OnInit {
 
+  molecule$!: Observable<MoleculeDetail>
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly moleculeService: MoleculeService
+  ) {}
+
+  ngOnInit(): void {
+    this.molecule$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        const molregno = params.get('molregno')
+        if (!molregno) throw new Error('UndefinedMolregno')
+        return this.moleculeService.getMoleculeByMolregno(molregno)
+      })
+    )
+  }
 }
