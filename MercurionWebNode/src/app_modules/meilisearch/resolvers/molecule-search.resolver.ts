ec5507e2fe3eb880eab8e02aaee64c3a553b54f0
giuliomarinelli <giulio.marinelli@icloud.com>
2025-05-11 12:24:32 +0200

    NEST | Fix => Set molecule search public

--- MercurionWebNode/src/app_modules/meilisearch/resolvers/molecule-search.resolver.ts
+++ MercurionWebNode/src/app_modules/meilisearch/resolvers/molecule-search.resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Args } from '@nestjs/graphql';
 import { MoleculeSearchService } from '../services/molecule-search.service';
 import { MoleculeSearchResult } from '../Models/DTO/molecule-search-result.cls';
 import { MoleculeSearchInput } from '../Models/DTO/molecule-search-input.cls';
+import { Public } from 'src/metadata/metadata';
 
 
 
@@ -9,6 +10,7 @@ import { MoleculeSearchInput } from '../Models/DTO/molecule-search-input.cls';
 export class MoleculeSearchResolver {
   constructor(private readonly moleculeSearchService: MoleculeSearchService) {}
 
+  @Public()
   @Query(() => [MoleculeSearchResult])
   async moleculeSearch(
     @Args('input') input: MoleculeSearchInput,
