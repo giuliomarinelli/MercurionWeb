e38bc6c49e5e3b4ca06583b2d19c534c95cef619
giuliomarinelli <giulio.marinelli@icloud.com>
2025-05-25 14:46:22 +0200

    NG | Custom scrollbar TODO: WCAG

--- MercurionWebNg/src/app/components/common/sidenav/sidenav.component.ts
+++ MercurionWebNg/src/app/components/common/sidenav/sidenav.component.ts
@@ -15,8 +15,7 @@ interface HistoryItem {
   imports: [RouterLink, DatePipe],
   template: `
     <nav class="flex flex-col h-full bg-transparent z-50 select-none">
-      <!-- Macro Area Menu -->
-      <header class="flex justify-end items-center gap-3 pt-4 mb-3 pr-[10px]">
+      <header class="flex justify-end items-center gap-3 pt-4 mb-3 pr-[10px] sticky top-0">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="fill-current w-6 h-auto text-light-on-surface-main dark:text-dark-on-surface-main">
           <!--!Font Awesome Pro 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2025 Fonticons, Inc.--><path d="M418.4 157.9c35.3-8.3 61.6-40 61.6-77.9c0-44.2-35.8-80-80-80c-43.4 0-78.7 34.5-80 77.5L136.2 151.1C121.7 136.8 101.9 128 80 128c-44.2 0-80 35.8-80 80s35.8 80 80 80c12.2 0 23.8-2.7 34.1-7.6L259.7 407.8c-2.4 7.6-3.7 15.8-3.7 24.2c0 44.2 35.8 80 80 80s80-35.8 80-80c0-27.7-14-52.1-35.4-66.4l37.8-207.7zM156.3 232.2c2.2-6.9 3.5-14.2 3.7-21.7l183.8-73.5c3.6 3.5 7.4 6.7 11.6 9.5L317.6 354.1c-5.5 1.3-10.8 3.1-15.8 5.5L156.3 232.2z"/>
         </svg>
@@ -25,6 +24,7 @@ interface HistoryItem {
           <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 368l0-24 0-64-64 0-24 0 0-48 24 0 64 0 0-64 0-24 48 0 0 24 0 64 64 0 24 0 0 48-24 0-64 0 0 64 0 24-48 0z"/>
         </svg>
       </header>
+      <!-- Macro Area Menu -->
       <h6 class="detail">Funzionalità</h6>
       <div class="mb-4">
         <a class="sidebar-link" routerLink="/collections">
@@ -64,7 +64,7 @@ interface HistoryItem {
       <hr class="border-slate-300 dark:border-slate-600 my-2"/>
 
       <!-- Cronologia dinamica -->
-      <div class="flex-1 overflow-y-auto">
+      <div class="flex-1 overflow-y-auto custom-scrollbar">
         <h6 class="detail ">Cronologia</h6>
         @if (historyItems.length) {
           <ul>
@@ -89,7 +89,11 @@ interface HistoryItem {
         @else {
           <p class="text-xs opacity-60 px-2 py-4">Nessuna attività recente.</p>
         }
+        <div>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
       </div>
+      </div>
+
     </nav>
   `,
   styles: [`
