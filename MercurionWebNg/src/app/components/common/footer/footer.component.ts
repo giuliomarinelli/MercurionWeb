1d556e2a5ac79c7d01bcc0ee28d6e73d35c23105
giuliomarinelli <giulio.marinelli@icloud.com>
2025-06-03 18:32:21 +0200

    NG | UX / UI improvements

--- MercurionWebNg/src/app/components/common/footer/footer.component.ts
+++ MercurionWebNg/src/app/components/common/footer/footer.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
+import { ThemeManagerService } from '../../../services/stores/theme-manager.service';
+import { PublicPipe } from '../../../pipes/public.pipe';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgOptimizedImage, PublicPipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  protected logoSrc = signal<string>('')
+
+  constructor(private readonly themeManager: ThemeManagerService) {
+    effect(() => this.logoSrc.set(this.themeManager.theme() === 'dark' ? 'logo/pictogram-dark-logo-2.svg' : 'logo/pictogram-light-logo.svg'))
+  }
 
 }
