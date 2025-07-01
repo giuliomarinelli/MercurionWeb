9e491268eecf90cf10cf15dba808a24f32dd964b
giuliomarinelli <122180544+giuliomarinelli@users.noreply.github.com>
2025-07-01 18:20:59 +0200

    Mock repository providers in ChEMBL item service spec

--- MercurionWebNode/src/app_modules/user/services/molecule-collection/chembl-molecule-item.service.spec.ts
+++ MercurionWebNode/src/app_modules/user/services/molecule-collection/chembl-molecule-item.service.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChEMBLMoleculeItemService } from './chembl-molecule-item.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ChEMBLMoleculeItemEntity } from '../../Models/entities/molecule-collection/chembl-molecule-item.entity';
+import { MoleculeCollection } from '../../Models/entities/molecule-collection/molecule-collection.entity';
+import { MoleculeCollectionItemJoinService } from './molecule-collection-item-join.service';
 
 describe('ChemblMoleculeItemService', () => {
   let service: ChEMBLMoleculeItemService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChEMBLMoleculeItemService],
+      providers: [
+        ChEMBLMoleculeItemService,
+        {
+          provide: MoleculeCollectionItemJoinService,
+          useValue: { add: jest.fn(), remove: jest.fn() },
+        },
+        {
+          provide: getRepositoryToken(ChEMBLMoleculeItemEntity),
+          useValue: {
+            findOne: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            createQueryBuilder: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(MoleculeCollection),
+          useValue: { findOne: jest.fn() },
+        },
+      ],
     }).compile();
 
     service = module.get<ChEMBLMoleculeItemService>(ChEMBLMoleculeItemService);
