df40b146d435d8613f1cc3951c47cc9b68a2cf74
giuliomarinelli <122180544+giuliomarinelli@users.noreply.github.com>
2025-07-10 22:36:59 +0200

    test: provide WsGuard deps in gateway spec

--- MercurionWebNode/src/app_modules/socket.io/socket.io.gateway.spec.ts
+++ MercurionWebNode/src/app_modules/socket.io/socket.io.gateway.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SocketIOGateway } from './socket.io.gateway';
 import { PubSubService } from '../redis/services/pub-sub.service';
+import { WsGuard } from './guards/ws.guard';
+import { JwtToolsService } from 'src/app_modules/auth/services/jwt-tools.service';
+import { SessionService } from 'src/app_modules/auth/services/session.service';
+import { Reflector } from '@nestjs/core';
+import { SecureCookieService } from 'src/app_modules/auth/services/secure-cookie.service';
 
 describe('SocketGateway', () => {
   let gateway: SocketIOGateway;
@@ -10,6 +15,11 @@ describe('SocketGateway', () => {
       providers: [
         SocketIOGateway,
         { provide: PubSubService, useValue: { setSocketServer: jest.fn() } },
+        WsGuard,
+        { provide: JwtToolsService, useValue: {} },
+        { provide: SessionService, useValue: {} },
+        { provide: Reflector, useValue: { get: jest.fn() } },
+        { provide: SecureCookieService, useValue: {} },
       ],
     }).compile();
 
