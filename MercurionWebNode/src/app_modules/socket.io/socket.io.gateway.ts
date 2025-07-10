e6dedda3bd3ec51335458a85ea6ede575fe08ee7
giuliomarinelli <giulio.marinelli@icloud.com>
2025-07-10 22:13:21 +0200

    NEST | fix

--- MercurionWebNode/src/app_modules/socket.io/socket.io.gateway.ts
+++ MercurionWebNode/src/app_modules/socket.io/socket.io.gateway.ts
@@ -1,4 +1,4 @@
-import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, MessageBody, ConnectedSocket, WsResponse } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 // import { randomUUID, UUID } from 'crypto';
 // import { nullish } from 'src/Models/nullish.type';
