0d375e7c3caf8fcfdf9f05a6df27794877ba4f67
giuliomarinelli <giulio.marinelli@icloud.com>
2025-10-10 10:50:42 +0200

    NG | Collections infinite scroll

--- MercurionWebNg/src/app/pages/my-molecule-collections/my-molecule-collections.component.ts
+++ MercurionWebNg/src/app/pages/my-molecule-collections/my-molecule-collections.component.ts
@@ -1,37 +1,68 @@
-import { Subscription } from 'rxjs';
+import { MoleculeCollection } from './../../Models/graphql/molecule-collection/molecule-collection.types';
+import { debounce, firstValueFrom, interval, Subscription } from 'rxjs';
 import { MyMoleculesHeadingComponent } from './../../components/molecule-detail/my-molecules-heading/my-molecules-heading.component';
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MoleculeCollectionService } from '../../services/graphql/molecule-collection.service';
+import { PageModel } from '../../Models/graphql/page.model';
+import { CollectionCardComponent } from '../../components/molecule-detail/collection-card/collection-card.component';
+import { ClassicSpinnerComponent } from '../../components/common/classic-spinner/classic-spinner.component';
 
 @Component({
   selector: 'app-my-molecule-collections',
-  imports: [MyMoleculesHeadingComponent],
+  imports: [MyMoleculesHeadingComponent, CollectionCardComponent, ClassicSpinnerComponent],
   template: `
 
   <section class="max-w-5xl mx-auto p-0 xs:p-4 sm:p-6 md:p-8 space-y-12">
     <app-my-molecules-heading />
-    <h1 id="molecule-name"
-        class="text-3xl md:text-4xl lg:text-[2.65rem] font-semibold tracking-wider text-center sm:text-left text-light-accent-primary dark:text-dark-accent-primary">
+    <h2 class="text-3xl md:text-4xl lg:text-[2.65rem] font-semibold tracking-wider text-center sm:text-left text-light-accent-primary dark:text-dark-accent-primary mb-2" style="margin-block-start: 0">
         Collezioni molecolari
-    </h1>
+    </h2>
+    <div class="mb-2"></div>
+    @for (item of items; track item; let i = $index) {
+      <app-collection-card [collection]="item" [i]="i" />
+    }
+    <div #sentinel class="sentinel"></div>
+    @if (loading) {
+      <div class="flex justify-center">
+        <app-classic-spinner [size]="60" />
+      </div>
+    }
   </section>
 
   `
 })
 export class MyMoleculeCollectionsComponent implements OnInit, OnDestroy, AfterViewInit {
 
+  // ======================= DEPS =======================
+  private readonly moleculeCollectionService = inject(MoleculeCollectionService)
+  // ====================================================
+
+
+
+  @ViewChild('sentinel', { static: true })
+  sentinel!: ElementRef;
+
   private pageSub?: Subscription
-  items: string[] = []
+  items: MoleculeCollection[] = []
   loading = false
   done = false
   private observer?: IntersectionObserver
-  private page = 0
+  private page = 1
 
   ngAfterViewInit(): void {
+    this.observer = new IntersectionObserver(
+      entries => {
+        const entry = entries[0];
+        if (entry.isIntersecting) this.loadMore();
+      },
+      { root: null, rootMargin: '0px 0px 200px 0px', threshold: 0 }
+    );
 
+    this.observer.observe(this.sentinel.nativeElement);
   }
 
   ngOnInit(): void {
-
+    this.loadMore()
   }
 
   ngOnDestroy(): void {
@@ -41,16 +72,21 @@ export class MyMoleculeCollectionsComponent implements OnInit, OnDestroy, AfterV
   async loadMore() {
     if (this.loading || this.done) return;
 
-    this.loading = true;
+    if (!this.done) {
+      this.loading = true;
+    }
 
-    // Simulazione fetch (puoi sostituire con una vera API)
+    const newPage = await firstValueFrom(
+      this.moleculeCollectionService.getPaginatedCollections(this.page, 6)
+        .pipe(
+          debounce(() => interval(200))
+        )
+    )
 
-    const newItems = Array.from({ length: 10 }, (_, i) => `Elemento ${(this.page * 10) + i + 1}`);
-
-    if (newItems.length === 0) {
+    if (newPage.items.length === 0) {
       this.done = true;
     } else {
-      this.items = [...this.items, ...newItems];
+      this.items = [...this.items, ...newPage.items];
       this.page++;
     }
 
