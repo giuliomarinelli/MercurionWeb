1c7225de65402a1e3521a03409c79218da470f05
giuliomarinelli <giulio.marinelli@icloud.com>
2025-11-19 01:33:36 +0100

    B_191125_554ECF8167544BE2D01E7C4C968CC49E

--- MercurionWebNg/src/app/services/socket.IO/realtime-socket.service.ts
+++ MercurionWebNg/src/app/services/socket.IO/realtime-socket.service.ts
@@ -6,7 +6,7 @@ import { io, Socket } from 'socket.io-client';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { JwtHelperService } from '../jwt-helper.service';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment.testing';
 
 export type SocketMode = 'public' | 'private';
 
