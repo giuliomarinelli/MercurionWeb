dde2f0c59ff33cc589f812ba71be4821c0877922
giuliomarinelli <giulio.marinelli@icloud.com>
2025-12-08 13:29:57 +0100

    NEST | Fixed failing unit test

--- MercurionWebNode/src/app_modules/notification/services/mail-sender/mail-sender.service.spec.ts
+++ MercurionWebNode/src/app_modules/notification/services/mail-sender/mail-sender.service.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailSenderService } from './mail-sender.service';
 import { MailerService } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from 'src/app_modules/user/services/user.service';
+import { MeiliLoggerService } from 'src/app_modules/meilisearch/services/meili-logger.service';
 
 describe('MailSenderService', () => {
   let service: MailSenderService;
@@ -10,6 +13,20 @@ describe('MailSenderService', () => {
       providers: [
         MailSenderService,
         { provide: MailerService, useValue: { sendMail: jest.fn() } },
+        { provide: ConfigService, useValue: { get: jest.fn().mockReturnValue('support@example.com') } },
+        {
+          provide: UserService,
+          useValue: {
+            getUserFirstNameById: jest.fn(),
+            getUserProvidedEmailById: jest.fn(),
+          },
+        },
+        {
+          provide: MeiliLoggerService,
+          useValue: {
+            forContext: jest.fn().mockReturnValue({ warn: jest.fn() }),
+          },
+        },
       ],
     }).compile();
 
