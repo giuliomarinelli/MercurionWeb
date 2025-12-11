befb49f1671329d3d2f1a2182fd3f231a1ba3e11
giuliomarinelli <giulio.marinelli@icloud.com>
2025-12-11 16:07:42 +0100

    NG | F_111225_7412A2328F78F8118946115E05FC9988 => Bugfix in itinere: le landing di errore andavano in auto redirect alla root. RISOLTO

--- MercurionLandingFactory/src/app/landing.page.ts
+++ MercurionLandingFactory/src/app/landing.page.ts
@@ -35,7 +35,7 @@ import { LandingPageConfig } from './Models/landing-page.config';
           (click)="goHome()"
           class="cursor-pointer rounded-md bg-light-accent-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm bg-[#2563eb] dark:bg-[#0b6de5] hover:bg-blue-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-dark-accent-primary-btn dark:hover:bg-blue-400/85 dark:focus-visible:outline-indigo-500 transition-colors duration-300"
         >
-          {{ vm().primaryCtaLabel }}[]
+          {{ vm().primaryCtaLabel }}
         </button>
       </div>
     </div>
