d5e71676c60a307a655756252e3c5cf38ef24582
giuliomarinelli <giulio.marinelli@icloud.com>
2025-12-15 19:59:29 +0100

    NG | F_151225_7F0AFF9FACC8C47A4BC265952F18F856 => CB2C0C8C9B81ECD1A48AFC6C877A835C: ticket detail from url OK

--- MercurionWebNg/src/app/services/context/action-context/action-overlay-context.service.ts
+++ MercurionWebNg/src/app/services/context/action-context/action-overlay-context.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, signal, effect, inject } from '@angular/core';
-import { ActionScope } from '../../../Models/action/action-overlay.models';
-
+import { Injectable, signal, effect } from '@angular/core'
+import { ActionScope } from '../../../Models/action/action-overlay.models'
 
 @Injectable({ providedIn: 'root' })
 export class ActionOverlayContextService {
@@ -9,6 +8,8 @@ export class ActionOverlayContextService {
   private _isMounted = signal<boolean>(false)
   private _isVisible = signal<boolean>(false)
   private _scope = signal<ActionScope>('')
+
+  private _pendingScope = signal<ActionScope | null>(null)
 
   readonly isOpened = this._isOpened.asReadonly()
   readonly isVisible = this._isVisible.asReadonly()
@@ -25,24 +26,46 @@ export class ActionOverlayContextService {
         setTimeout(() => this._isMounted.set(false), 300)
       }
     })
+
+    effect(() => {
+      const mounted = this._isMounted()
+      const pending = this._pendingScope()
+
+      if (!mounted || !pending) {
+        return
+      }
+
+      this._pendingScope.set(null)
+
+      queueMicrotask(() => {
+        this._scope.set(pending)
+        this._isOpened.set(true)
+      })
+    })
   }
 
   open(scope: ActionScope) {
+    if (!this._isMounted()) {
+      this._pendingScope.set(scope)
+      this._isOpened.set(true)
+      return
+    }
+
     this._scope.set(scope)
     this._isOpened.set(true)
   }
 
   close() {
     queueMicrotask(() => {
+      this._pendingScope.set(null)
       this._isOpened.set(false)
       setTimeout(() => this._scope.set(''), 500)
     })
   }
 
-  switchToScope(scope: ActionScope): void {
+  switchToScope(scope: ActionScope) {
     queueMicrotask(() => {
       this._scope.set(scope)
     })
   }
-
 }
