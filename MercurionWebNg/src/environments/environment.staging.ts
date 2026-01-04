f6f9de37949c845c20a4a8294abcd2ebd38ba3e6
giuliomarinelli <giulio.marinelli@icloud.com>
2026-01-04 16:47:45 +0100

    KUBERNETES | DOCKER | Edge endpoint conf, NAT conf, various bugfixes

--- MercurionWebNg/src/environments/environment.staging.ts
+++ MercurionWebNg/src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  CLOUDFLARE_SITE_KEY: '0x4AAAAAACKT6W62iJkO8S_f',
+  CLOUDFLARE_SITE_KEY: '0x4AAAAAABdOWnfz_3r4JGDK',
   PUBLIC_EXACT_PATHS: [
     '/login',
     '/register',
